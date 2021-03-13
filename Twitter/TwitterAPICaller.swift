7bb400791fe648c27395754e6fb7b515c4bc9ad1
Manniec <manniec00@gmail.com>
2021-03-13 05:47:09 -0800

    draft unfavorite function in twitter api

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -85,4 +85,15 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
 
     }
     
+    func unfavoriteTweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/favorites/destroy.json"
+        
+        TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+            success()
+        }, failure: { (task: URLSessionDataTask?, error: Error) in
+            failure(error)
+        })
+
+    }
+    
 }
